5e80e5e1dd472e7d02e73175bcf66faed8044e5d
Adrien Combier <acombier@z4r10p5.42lyon.fr>
2025-11-12 14:45:11 +0100

    strdup add

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	dest = malloc(sizeof(char) * i + 1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int main(void)
+{
+	char	src[] = "Hello 42";
+
+	printf("%s", ft_strdup(src));
+
+	return (0);
+}*/
