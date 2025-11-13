fb1a3e67c96de4be50d010a26e439af42b2877bc
Adrien COMBIER <adrien.combier21@gmail.com>
2025-11-13 20:09:52 +0100

    Push of ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itao.c                                          :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acombier <acombier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adriencombier <adriencombier@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 14:36:02 by acombier          #+#    #+#             */
-/*   Updated: 2025/11/13 14:59:33 by acombier         ###   ########.fr       */
+/*   Updated: 2025/11/13 20:05:18 by adriencombi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_intlen(int n)
+int	ft_intlen(long n)
 {
 	int	len;
 
@@ -34,9 +34,45 @@ int	ft_intlen(int n)
 	}
 	return (len);
 }
+void	rec_itoa(long nb, char *str, long *i)
+{
+	if(nb >= 10)
+		rec_itoa(nb / 10, str, i);
+	str[*i] = nb % 10 + '0';
+	(*i)++;
+}
 
 char	*ft_itoa(int n)
 {
-	if(n == -2147483648)
-	{}
+	int long nb;
+	int long len;
+	long	i;
+	
+	i = 0;
+	nb = n;
+	len = ft_intlen(nb);
+	char	*str = malloc(sizeof(char) * (len + 1));
+	if(!str)
+		return (NULL);
+		str[len] = '\0';	
+	if(nb < 0)
+	{
+		nb *= -1;
+		str[0] = '-';
+	}
+	if(nb == 0)
+	{
+		str[0] = '0';
+		str[1] = '\0';
+		return(str);
+	}
+	rec_itoa(nb, str, &i);
+	return (str);
+}
+
+int main(void)
+{
+	printf("%s", ft_itoa(730));
+
+	return (0);
 }
